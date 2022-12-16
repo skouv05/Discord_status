04a61074cc7b058d9e192f659ef747af44de1871
skouv05 <jakobskouv@gmail.com>
2022-12-16 12:54:19 +0100

    host

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ async def hello_world(id):
 
 if __name__ == "__main__":
     port = os.environ.get("PORT")
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True, host="0.0.0.0")
     run()
